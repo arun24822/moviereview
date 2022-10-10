f7a596e784fe19034068a6d0f3216e1fa1507a03
Arun Kumar <arun24822@gmail.com>
2022-10-10 19:52:58 +0530

    updated settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -152,4 +152,4 @@ LOGIN_URL = 'signinaccount'
 django_on_heroku.settings(locals())
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
