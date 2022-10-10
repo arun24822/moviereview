533a51f73c731bcb3b3711069430423cca85c67e
Arun Kumar <arun24822@gmail.com>
2022-10-10 06:53:03 +0530

    modified settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com']
 
 
 # Application definition
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'arunkumar',
+        'USER': 'movieuser',
+        'PASSWORD': 'movieuser123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
