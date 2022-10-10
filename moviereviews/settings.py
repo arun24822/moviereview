ca1951adf3356705c3aeebe614da018a60294631
Arun Kumar <arun24822@gmail.com>
2022-10-10 07:21:15 +0530

    updated setting and procfile

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'moviereviews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'arunkumar',
         'USER': 'movieuser',
         'PASSWORD': 'movieuser123',
