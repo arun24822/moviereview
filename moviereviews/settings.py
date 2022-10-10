4db92f87f7cd0b62e25b96f6007a08420608d7c1
Arun Kumar <arun24822@gmail.com>
2022-10-10 19:38:45 +0530

    updated settings.py

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -144,7 +144,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'moviereviews/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 LOGIN_URL = 'signinaccount'
@@ -152,4 +152,4 @@ LOGIN_URL = 'signinaccount'
 django_on_heroku.settings(locals())
 
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
